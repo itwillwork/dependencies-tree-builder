63bdcc0ee8e7b0c941cac344c1b4cc3ad947e076
Dmitriy Karasev <>
2019-03-29 19:30:43 +0300

    🐛 Fix bug JavaScript heap out of memory, add variable maxDepth

--- src/tree_factory.js
+++ src/tree_factory.js
@@ -1,12 +1,13 @@
 const semver = require('semver');
 
 class TreeFactory {
-  constructor(packageCollector, logger) {
+  constructor(packageCollector, logger, maxDepth) {
     this._packageCollector = packageCollector;
     this._logger = logger;
+    this._maxDepth = maxDepth;
   }
 
-  async create(rawNode, breadcrumbs = []) {
+  async create(rawNode, breadcrumbs = [], depth = new Map()) {
     if (!rawNode) {
       this._logger.log('rawNode', rawNode, breadcrumbs);
       return {};
@@ -16,17 +17,24 @@ class TreeFactory {
 
     const dependencies = await Promise.all(
       rawNodeDependencies.map(async ([name, version]) => {
-        if (!semver.valid(version)) {
-          this._logger.log(`skip ${name}@${version}`);
+        const dependency_name = `${name}@${version}`;
 
-          return `skip ${name}@${version}`;
+        if (!semver.valid(version)) {
+          this._logger.log(`skip ${dependency_name}`);
+
+          return `skip ${dependency_name}`;
         }
+
+        const name_depth = `${fullName} -> ${dependency_name}`;
+        const current_depth = depth.get(name_depth);
+        if (current_depth) { depth.set(name_depth, current_depth + 1); } else { depth.set(name_depth, 1); }
+        if (current_depth > this._maxDepth) return `skip ${dependency_name}`;
 
         const dependency = await this._packageCollector.get(name, version);
 
         const nextBreadcrumbs = [...breadcrumbs, fullName];
 
-        return await this.create(dependency, nextBreadcrumbs);
+        return await this.create(dependency, nextBreadcrumbs, depth);
       })
     );
 
